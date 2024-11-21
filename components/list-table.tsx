43446de813c60463b0aeda9b2536f6b5c895f659
Adithya Krishnan <krishsandeep@gmail.com>
2024-11-22 00:53:53 +0100

    add search

--- components/list-table.tsx
+++ components/list-table.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Search } from "lucide-react";
 import {
     Table,
     TableBody,
@@ -10,12 +11,16 @@ import {
 
 import {
     ColumnDef,
+    ColumnFiltersState,
     SortingState,
     flexRender,
     getCoreRowModel,
+    getFilteredRowModel,
     getSortedRowModel,
     useReactTable,
 } from "@tanstack/react-table";
+
+import { Input } from "@/components/ui/input";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
@@ -27,20 +32,42 @@ export default function ListTable<TData, TValue>({
     data,
 }: DataTableProps<TData, TValue>) {
     const [sorting, setSorting] = React.useState<SortingState>([]);
+    const [columnFilters, setColumnFilters] =
+        React.useState<ColumnFiltersState>([]);
     const table = useReactTable({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
         onSortingChange: setSorting,
         getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
         state: {
             sorting,
+            columnFilters,
         },
     });
     return (
-        <div className="w-full flex justify-center px-1 sm:px-5 md:px-10 lg:px-20 pt-5">
-            <div className="mx-auto max-w-7xl overflow-x-auto border rounded-lg">
-                <Table>
+        <div className="w-full flex flex-col justify-center px-2 sm:px-5 md:px-10 lg:px-20 pt-5">
+            <div className="mx-auto max-w-7xl w-full flex flex-row items-center justify-start py-4">
+                <Search className="w-5 h-5 mr-3" />
+                <Input
+                    placeholder="Search products..."
+                    value={
+                        (table
+                            .getColumn("product")
+                            ?.getFilterValue() as string) ?? ""
+                    }
+                    onChange={(event) =>
+                        table
+                            .getColumn("product")
+                            ?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-md"
+                />
+            </div>
+            <div className="mx-auto max-w-7xl w-full overflow-x-auto border rounded-lg">
+                <Table className="w-full">
                     <TableHeader className="bg-zinc-100">
                         {table.getHeaderGroups().map((headerGroup) => (
                             <TableRow key={headerGroup.id}>
